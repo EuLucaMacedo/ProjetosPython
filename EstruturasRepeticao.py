3562ce8455f5f17db4239de12216998f8cbb563c
Luca Macedo <lucamacedo.lm@gmail.com>
2025-06-25 14:37:44 -0300

    Refatorando Códigos nas estruturas de Repetição e adicionando novas funcionalidades

--- EstruturasRepeticao.py
+++ EstruturasRepeticao.py
@@ -15,12 +15,14 @@ print("a lista chegou ao fim!")
 
 print('Vamos Calcular os Números!')
 mul = 0
+cont = 0
 print('Calculando ...')
 sleep(3)
 for c in range(1,500):
     if (c % 3 == 0 and c % 2 != 0):
         mul += c
-        
+        cont += 1
+print(f'Foram Registrados {cont} números!')
 print(f'A soma dos numeros Multiplos de 3 ente 1 e 500 é: {mul}')
 
 numero = int(input('Digite o Numero para a Tabuada: '))
@@ -38,10 +40,12 @@ print(f'A soma dos Numeros pares é {soma}')
 
 termo = int(input('Digite um Número: '))
 razao = int(input('Digite o Valor do Intervalos entre eles: '))
-
-for c in range(10):
-    print(f'{termo + c * razao} ... ')
+decimo = termo + (10 - 1) * razao
+for c in range(termo, decimo + razao, razao):
+    print(f'{c} ... ', end='')
 print('Fim da Sequencia!')
+
+
 ano_atual = datetime.datetime.now().year
 quan_menos = 0
 quan_mais = 0
@@ -74,9 +78,12 @@ media = 0
 homem_velho = ''
 
 for c in range(0, 4):
+    print(f'============= {c + 1}° Pessoa =============')
     nome = input('Digite o Seu Nome: ')
     idade = int(input('Qual a Sua Idade: '))
-    sexo = input('Qual o seu Sexo: [M/F]').upper()
+    sexo = input('Qual o seu Sexo: [M/F]: ').upper()
+
+    print("")
 
     if idade > idade_Compa and sexo == 'M':
         idade_Compa = idade
@@ -90,6 +97,22 @@ for c in range(0, 4):
 print(f'A Média entre as idades é igual a {media/4}!')
 print(f'O Nome do homem mais velho é {homem_velho}!')
 print(f'A Quantidade de Mulheres com menos de 21 é Igual a {idade_Femi} mulheres!')
+
+primo = int(input('Digite um Número: '))
+tot = 0
+for c in range(1, primo + 1):
+    if primo % c == 0:
+        print('\033[33m', end='')
+        tot += 1
+    else:
+        print('\033[31m', end='')
+    print(f'{c} ', end='')
+print(f'\n\033[mO número {primo} foi divisivel {tot} vezes!')
+if tot == 2:
+    print('E por isso ele é PRIMO!')
+else:
+    print('E Por isso ele NÂO é PRIMO!')
+
 
 
 
