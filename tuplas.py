0fcb643e1788ea26797b75a3b108e6aaa07aa571
Luca Macedo <lucamacedo.lm@gmail.com>
2025-07-05 00:34:20 -0300

    Organizando o Projeto

--- tuplas.py
+++ tuplas.py
@@ -1,4 +1,5 @@
 import random
+#Buscando Numeros na Tupla
 numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco')
 
 while True:
@@ -9,17 +10,20 @@ while True:
     else:
         print('Número inválido. Tente novamente.')
 
+#FAzendo analises com Tuplas
 times = ('Flamengo', 'Palmeiras', 'Corinthians', 'São Paulo', 'Atlético-MG', 'Grêmio', 'Internacional', 'Chapecoense', 'Bahia', 'Cruzeiro')
 print(f'Os 5 Primeiros Da Lista: {times[:5]}')
 print(f'Os Ultimos Quatro da Lista: {times[-4:]}')
 print(f'Lista em Ordem Alfabética: {sorted(times)}')
 print(f'O Chapecoence está na {times.index('Chapecoense') + 1}° Posição')
 
+#Sorteio com Tuplas
 sorteio = (random.sample(range(1,10), 5))
 print(f'os valores sorteados foram: {sorteio}')
 print(f'O maior valor sorteado foi {max(sorteio)}')
 print(f'O menor valor sorteado foi {min(sorteio)}')
 
+#Listas com Tuplas
 listagem = ('arroz', 1,'papel', 2, 'Biscoito', 7, 'Panela', 10)
 print('Listagem de Produtos')
 print('='*35)
@@ -27,6 +31,7 @@ for c in range(0, len(listagem), 2):
     print(f'{listagem[c]:<10}................ R${listagem[c + 1]:>7.2f}')
 print('='*35)
 
+#Analizando Strings com Tuplas
 palavras = ('gato', 'lingua', 'cachorro', 'peixe', 'Hamster')
 
 for palavras in palavras:
@@ -34,7 +39,7 @@ for palavras in palavras:
     for vogais in palavras:
         if vogais.lower() in 'aeiou':
             print(vogais, end=' ')
-
+print('Fim do Programa')
 
 #Inserindo Valores na Tupla
 
@@ -50,3 +55,5 @@ print('Valores Pares Digitados: ', end='')
 for c in núm:
     if c % 2 == 0:
         print(f'{c}... ', end='')
+
+print('Fim do Programa!')
