cb485696ba1ec31b84eef1578bbefd3d6ae6e2d4
Luca Macedo <lucamacedo.lm@gmail.com>
2025-07-14 21:52:34 -0300

    Adicionando mais Funções

--- listasematrizes.py
+++ listasematrizes.py
@@ -1,3 +1,7 @@
+import random
+import time
+from time import sleep
+
 dados = []
 nome  = []
 peso = []
@@ -45,3 +49,67 @@ elif contador_pessoa == 1:
     print(f'Somente Uma Pessoa Cadastrada. Peso de {maior_peso}Kg.')
 else:
     print('Não tem Pessoas Cadastradas!')
+from itertools import count
+
+#Lista com pares e ímpares
+
+lista_num = []
+lista_par = []
+lista_impar = []
+
+for c in range(7):
+    valor = int(input(f'Digite o {c + 1}° Valor: '))
+    lista_num.append(valor)
+
+    if valor % 2 == 0:
+        lista_par.append(lista_num)
+    else:
+        lista_impar.append(lista_num)
+print(f'Os Valores digitados foram {sorted(lista_num)}')
+print(f'Os Valores pares digitados foram: {sorted(lista_par)}' if lista_par > 0 else 'Não Existe Número Par na Lista!')
+print(f'Os valores ímpares digitados foram: {sorted(lista_impar)}' if lista_impar > 0 else 'Não Existe Número Ímpar na Lista!')
+
+#Matrizes
+linhas = 3
+colunas = 3
+lista_matriz = []
+valor_par = 0
+terceira_coluna = 0
+segunda_linha = 0
+
+for i in range(linhas):
+    linha = []
+    for j in range(colunas):
+        valor = int(input(f'Digite o Valor para [{i}][{j}]: '))
+        linha.append(valor)
+
+        if valor % 2 == 0:
+            valor_par += valor
+
+    lista_matriz.append(linha)
+for i in range(linhas):
+    terceira_coluna += lista_matriz[i][2]
+
+segunda_linha = max(lista_matriz[1])
+
+print('Matriz Digitada: ')
+for linha in lista_matriz:
+    print(linha)
+
+print(f'Soma dos Valores Pares: {valor_par}')
+print(f'Soma dos Valores da Terceira Coluna: {terceira_coluna}')
+print(f'Maior valor da Segunda Linha: {segunda_linha}')
+
+#Palpites para MegaSena
+palpite = int(input('Quantos Jogos você quer que eu sorteie? '))
+print(f'\n{'=' * 5}Sorteando {palpite} Jogos{'=' * 5}')
+
+for c in range(1, palpite + 1):
+    sorteio = sorted(random.sample(range(1, 61), 6))
+    print((f'Jogo {c}: {sorteio}'))
+    sleep(0.5)
+    
+print(f'\n{'=' * 5} BOA SORTE {'=' * 5}')
+
+
+
